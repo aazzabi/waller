96cf1265c3f8754652992283ef168bfd533aa800
Arafet Azzabi <aarefet@smart-team.tn>
2017-02-28 09:40:59 +0100

    [GC-1] Adding translations for Profile and candidature INDEX and SHOW

--- app/Resources/translations/messages.fr.php
+++ app/Resources/translations/messages.fr.php
@@ -1,11 +1,37 @@
 <?php
 return [
     'entity' => [
+        'common' => [
+            'libelle' => 'Libellé'
+        ],
         'profile' => [
+            'id' =>'ID',
+            'prenom' => 'Prenom',
+            'telephone' => 'Téléphone',
+            'email' => 'E-mail',
             'nom' => 'Nom',
-            'prestation'=> 'Prestation salariale',
-            'cv'=>'Curriculum vitae',
-            'photo'=>'Photo de profile'
+            'experience'=>'Années d\'experience',
+            'niveau'=>'Niveau',
+            'skype'=>'Skype',
+            'linkedin'=>'Linkedin',
+
+            'facebook'=>'Facebook',
+            'github'=>'Github',
+            'sivp'=>'SIVP',
+            'reseauxsociaux'=> 'Reseaux sociaux',
+            'prestationsalariale' => 'Prestation salariale',
+            'cv' => 'Curriculum vitae',
+            'photo' => 'Photo de profile'
+        ],
+        'candidat'=>[
+            'photo' => 'Photo du candidat',
+            'id' =>'ID',
+            'nom' => 'Nom',
+            'prenom' => 'Prenom',
+            'email' => 'E-mail',
+            'poste' => 'Poste',
+            'etapecourante' => 'Etape courante',
+            'actions' => 'Actions'
         ]
     ]
 ];
