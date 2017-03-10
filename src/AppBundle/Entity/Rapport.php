1351ea4efec56aa07196f7aaa3a80868344897e3
Arafet Azzabi <aarefet@smart-team.tn>
2017-03-10 15:24:58 +0100

    [GC-15] Listener pour la creation et update de rapportpour un candidat

--- src/AppBundle/Entity/Rapport.php
+++ src/AppBundle/Entity/Rapport.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Table(name="rapport")
  * @ORM\Entity
+ * @ORM\EntityListeners({"AppBundle\Entity\Listeners\RapportListener"})
  */
 class Rapport
 {
@@ -45,6 +46,36 @@ class Rapport
      * })
      */
     private $candidature;
+
+    /**
+     * @var User $userCreated
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_created", referencedColumnName="id", nullable=true)
+     */
+    private $userCreated;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_created", type="datetime", nullable=true)
+     */
+    private $dateCreated;
+
+    /**
+     * @var User $userUpdated
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="user_updated", referencedColumnName="id", nullable=true)
+     */
+    private $userUpdated;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_updated", type="datetime", nullable=true)
+     */
+    private $dateUpdated;
 
     /**
      * Get id
@@ -127,4 +158,70 @@ class Rapport
     {
         return $this->candidature;
     }
+
+    /**
+     * @return User
+     */
+    public function getUserCreated()
+    {
+        return $this->userCreated;
+    }
+
+    /**
+     * @param User $userCreated
+     */
+    public function setUserCreated($userCreated)
+    {
+        $this->userCreated = $userCreated;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateCreated()
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @param \DateTime $dateCreated
+     */
+    public function setDateCreated($dateCreated)
+    {
+        $this->dateCreated = $dateCreated;
+    }
+
+    /**
+     * @return User
+     */
+    public function getUserUpdated()
+    {
+        return $this->userUpdated;
+    }
+
+    /**
+     * @param User $userUpdated
+     */
+    public function setUserUpdated($userUpdated)
+    {
+        $this->userUpdated = $userUpdated;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateUpdated()
+    {
+        return $this->dateUpdated;
+    }
+
+    /**
+     * @param \DateTime $dateUpdated
+     */
+    public function setDateUpdated($dateUpdated)
+    {
+        $this->dateUpdated = $dateUpdated;
+    }
+
+
 }
