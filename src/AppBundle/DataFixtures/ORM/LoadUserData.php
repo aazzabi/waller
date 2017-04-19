cf8b3bceb157a5b8c8f4236ba53b562e5104fa5f
Walid Jlidi <walid.jlidi89@gmail.com>
2017-04-19 10:39:11 +0100

    Add fixture config and LoadUserData Class to add users fixutres to database

---
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -0,0 +1,69 @@
+<?php
+namespace Acme\AppBundle\DataFixtures\ORM;
+
+
+use AppBundle\Entity\User;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Faker;
+
+class LoadUserData
+    extends AbstractFixture
+    implements OrderedFixtureInterface, ContainerAwareInterface
+{
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $faker= Faker\Factory::create();
+        $userManager = $this->container->get('fos_user.user_manager');
+        for ($i=0;$i<20;$i++){
+            $user = $userManager->createUser();
+            $user->setUserName($faker->userName);
+            $user->setNom($faker->firstName);
+            $user->setPrenom($faker->lastName);
+            $user->setEmail($faker->email);
+            $user->setPlainPassword('123456');
+            $user->setEnabled(true);
+            $user->addRole($this->getRandomRole());
+            $userManager->updateUser($user);
+        }
+
+    }
+
+    /**
+     * Sets the container.
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function getRandomRole(){
+        $roles  = array('ROLE_ADMIN', 'ROLE_USER', 'ROLE_SUPER_ADMIN' );
+        return $roles[array_rand($roles)];
+    }
+
+
+    /**
+     * Get the order of this fixture
+     * @return integer
+     */
+    public function getOrder()
+    {
+        return 1;
+    }
+
+}
