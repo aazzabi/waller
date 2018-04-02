9d949ae7ca139393d04acf9fa95212e87950fdef
Walid MNASRI <wmnasri@smart-team.tn>
2018-04-02 18:37:52 +0100

    Add model managers

---
+++ src/AppBundle/ModelManager/EtapeManager.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @author  Rokbani Ghayth
+ */
+
+namespace AppBundle\ModelManager;
+
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * Manager of the entity Etape.
+ */
+class EtapeManager
+{
+    /**
+     *
+     * @var RegistryInterface
+     */
+    protected $doctrine;
+
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * Constructor class.
+     *
+     * @param RegistryInterface $doctrine Instance of doctrine.
+
+     */
+    public function __construct(RegistryInterface $doctrine)
+    {
+        $this->doctrine             = $doctrine;
+        $this->entityManager        = $this->doctrine->getManager();
+        $this->etapeRepository = $this->entityManager->getRepository("AppBundle:Etape");
+    }
+
+}
---
+++ src/AppBundle/ModelManager/PosteManager.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @author  Rokbani Ghayth
+ */
+
+namespace AppBundle\ModelManager;
+
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * Manager of the entity Poste.
+ */
+class PosteManager
+{
+    /**
+     *
+     * @var RegistryInterface
+     */
+    protected $doctrine;
+
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * Constructor class.
+     *
+     * @param RegistryInterface $doctrine Instance of doctrine.
+
+     */
+    public function __construct(RegistryInterface $doctrine)
+    {
+        $this->doctrine             = $doctrine;
+        $this->entityManager        = $this->doctrine->getManager();
+        $this->posteRepository = $this->entityManager->getRepository("AppBundle:Poste");
+    }
+
+}
