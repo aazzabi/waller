07b68968a01436516b9df6aacb682ce7ace14d63
Walid MNASRI <wmnasri@smart-team.tn>
2018-04-12 13:03:24 +0100

    Add use statement

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
-use AppBundle\Entity\Profile;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
@@ -59,7 +59,7 @@ class User extends BaseUser
     /**
      * Get profiles
      *
-     * @return Collection|Profile[]
+     * @return ArrayCollection|Profile[]
      */
     public function getProfiles()
     {
@@ -69,7 +69,7 @@ class User extends BaseUser
     /**
      * Get group
      *
-     * @return Group
+     * @return mixed
      */
     public function getGroup()
     {
